75ff920072c5dc095651cbb7115bd8eb0140041d
Connor Simmons <turkish@d4rkwolf.co.uk>
2026-05-18 09:42:22 +0100

    fix: add data migration for existing assured_shorthold tenancy rows

--- packages/db/migrations/0001_regular_santa_claus.sql
+++ packages/db/migrations/0001_regular_santa_claus.sql
@@ -1,1 +1,2 @@
 ALTER TABLE "tenancies" ALTER COLUMN "tenancy_type" SET DEFAULT 'periodic_assured';
+UPDATE "tenancies" SET "tenancy_type" = 'periodic_assured' WHERE "tenancy_type" = 'assured_shorthold';
